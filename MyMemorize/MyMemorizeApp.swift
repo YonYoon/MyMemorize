9bfdb76244464d641fe91098af93470611078e8a
YonYoon <zhzhzh1806@gmail.com>
2023-09-28 14:04:26 +0500

    Initial Commit

---
+++ MyMemorize/MyMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMemorizeApp.swift
+//  MyMemorize
+//
+//  Created by Zhansen Zhalel on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
